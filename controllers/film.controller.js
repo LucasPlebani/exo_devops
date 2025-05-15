60e6e4202769b5cf852b97289b6f322b29b5e94c
Lucas Plebani <lucas.plebani@outlook.fr>
2025-05-15 23:11:55 +0200

    debug

--- controllers/film.controller.js
+++ controllers/film.controller.js
@@ -1,7 +1,7 @@
 const filmModel = require("../models/film.model");
 
 exports.createFilm = async (req, res) => {
-    const { title, description, director, releaseDate } = req.body;
+    const { title, description, director, release_year } = req.body; // release_year ici
     const user_id = req.user.id;
 
     try {
@@ -9,7 +9,7 @@ exports.createFilm = async (req, res) => {
             title,
             description,
             director,
-            releaseDate,
+            release_year,
             user_id
         );
         res.status(201).json(film);
@@ -31,8 +31,7 @@ exports.getFilmById = async (req, res) => {
     const { id } = req.params;
     try {
         const film = await filmModel.getFilmById(id);
-        if (!film)
-            return res.status(404).json({ message: "Film introuvable" });
+        if (!film) return res.status(404).json({ message: "Film introuvable" });
         res.status(200).json(film);
     } catch (err) {
         res.status(500).json({ message: err.message });
@@ -41,7 +40,7 @@ exports.getFilmById = async (req, res) => {
 
 exports.updateFilm = async (req, res) => {
     const { id } = req.params;
-    const { title, description, director, releaseDate } = req.body;
+    const { title, description, director, release_year } = req.body; // release_year ici aussi
     const user_id = req.user.id;
 
     try {
@@ -50,7 +49,7 @@ exports.updateFilm = async (req, res) => {
             title,
             description,
             director,
-            releaseDate,
+            release_year,
             user_id
         );
         if (!updated)
