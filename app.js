b5ec0436d57018ac1bd8a45457cab7b8e14cfb38
Lawrence Terpin <lawrence-terpin@MacBook-Pro-de-Lawrence.local>
2025-05-15 11:25:55 +0200

    feat: modification du texte de bieenvenue nodejs

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => res.send("Projet Nodejs avec Docker !"));
+app.get("/", (req, res) =>
+  res.send("Hello, projet Docker nodejs avec Railway !")
+);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
